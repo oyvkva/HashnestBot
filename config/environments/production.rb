abeed16630e0245151349e31da2260ad55380ff8
Øyvind Kvanes <oyvind@kvanes.no>
2016-01-04 15:57:14 +0100

    Add Google Analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  # replace this with your production tracker code
+  GA.tracker = "UA-71975820-1"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
