34ce0606a144e08bb071aedaf805d96cb054f0c8
Oyvkva <oyvind@kvanes.no>
2017-05-12 10:30:28 +0200

    Fix tests

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,6 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.price_notifiaction.subject
   #
   def price_notifiaction(new_price,string)
-    mail to: "oyvind@kvanes.no", subject: "Price notification for #{string}, price is #{new_price}", body: "Price notification for #{string}, price is #{new_price}"
+    mail to: "to@example.org", subject: "Price notification for #{string}, price is #{new_price}", body: "Price notification for #{string}, price is #{new_price}"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "price_notifiaction" do
-    mail = UserMailer.price_notifiaction
-    assert_equal "Price notifiaction", mail.subject
+    mail = UserMailer.price_notifiaction(0.01, "BTC")
+    assert_equal "Price notification for BTC, price is 0.01", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Price notification for BTC, price is 0.01", mail.body.encoded
   end
 
 end
